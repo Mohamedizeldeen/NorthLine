0ed644d61c8270cc2c6d355ee6c8817a4c2d071c
Mohamed Izeldeen <eng.mohamed.izeldeen@gmail.com>
2025-01-30 01:46:48 -0800

    Added .gitignore to exclude node_modules

---
+++ index.js
@@ -0,0 +1,14 @@
+const express = require('express');
+
+app = express();
+port = 3000;
+
+
+app.get('/', (req, res) => {
+
+    res.send('Hello World');
+});
+
+app.listen(port, () => {
+    console.log(`Server is running on port ${port}`);
+});
